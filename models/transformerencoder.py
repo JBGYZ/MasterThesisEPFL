04e5369371411fba8061be4aa1821e5319e21c39
Guanyu Zhang <51890833+JBGYZ@users.noreply.github.com>
2023-10-26 11:48:44 +0200

    fixed bugs in learned positonal encoding

--- models/transformerencoder.py
+++ models/transformerencoder.py
@@ -45,14 +45,18 @@ class LearnedPositionalEncoding(nn.Module):
     """
         learned positional encoding for short sequences.
     """
-    def __init__(self, d_model, max_seq_len=512):
+    def __init__(self, d_model, max_seq_len=32):
         super(LearnedPositionalEncoding, self).__init__()
         self.position_embeddings = nn.Embedding(max_seq_len, d_model)
 
     def forward(self, x):
-        seq_len = x.size(1)
-        positions = torch.arange(seq_len, device=x.device).expand(x.size(0), seq_len)
-        position_embeddings = self.position_embeddings(positions)
+        """
+        Arguments:
+            x: Tensor, shape ``[seq_len, batch_size, embedding_dim]``
+        """
+        seq_len = x.size(0)
+        positions = torch.arange(seq_len, device=x.device).expand(x.size(1), seq_len)
+        position_embeddings = self.position_embeddings(positions).permute(1, 0, 2)
         x = x + position_embeddings
         return x
 
